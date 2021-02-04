56afd1c5c9744595cd7de4a124182ffb2403d89c
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-04 17:55:52 +0200

    Operaciones matemáticas

---
+++ op-asignacion.py
@@ -0,0 +1,30 @@
+#Suma
+a = 10
+a += 20
+# print(a)
+
+#Resta
+b = 20
+b -=5
+# print(b)
+
+#Modulo
+c = 10
+c %= 5
+# print(c)
+
+#División
+d = 10
+d /= 3
+# print(int(d))
+
+#Potencia
+e = 10
+e **= 2
+print('El valore de la potencia es:', e)
+
+
+#Multiplicar
+f = 20
+f *= 3
+print(f)
