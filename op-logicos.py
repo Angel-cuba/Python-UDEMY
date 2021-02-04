e281bdc1563a227c9f3066bfb5423d3670292b57
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-04 17:45:16 +0200

    Ejemplos con OR y AND

---
+++ op-logicos.py
@@ -0,0 +1,27 @@
+# #Operador not
+# print(not False)
+
+# #operador And
+# print(False and True)
+
+# #operador or
+# print(True or False)
+
+# #Example
+# c = "Python"
+# print(len(c))
+# if len(c) > 8:
+#           print('es mayor')
+# else:
+#           print('es menor')
+    
+print("Sistemas de becas")
+
+kilo = int(input("Cuanto kilometro para caminar hacia la escuela: "))
+cantidad = int(input("Cuanto hay en la familia: "))
+ingreso = int(input("Cantidad de dinero en la familia: "))
+
+if kilo < 10 and cantidad < 3 and ingreso > 2000:
+          print("Derecho a tener beca")
+else:
+          print("No ha beca para ti")        
--- op-relacionales.py
+++ op-relacionales.py
@@ -1,3 +1,25 @@
+
+#Operador ==
 c = 5 == 5
 print(c)
-print('El valor de c es: ' , c) 
+print('El valor de c es: ' , c)
+
+#Operador  !=
+
+# a = 4 != 5
+# b = "Angel" != "Luis"
+# print(a)
+# print(b)
+
+
+
+#Operador menor y mayor
+a = 5 < 3 
+b = 5 > 3
+print(a,b)
+
+#Operador menor o igual
+d = 20 <= 10
+e = 20 >= 10
+f = 20 >= 20
+print(d, e, f)
