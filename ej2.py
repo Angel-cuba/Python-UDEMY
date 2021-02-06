782f43a58de640b1f355709c31156cd2d8995c4a
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-06 13:32:56 +0200

    ejercicios 2

---
+++ ej2.py
@@ -0,0 +1,22 @@
+asignatura = ["matemática", "física", "lengua", "historia"]
+print(asignatura)
+
+for i in asignatura:
+          print("Estudio " + i + " en la escuela")
+
+nombre = input("nombre: ")
+edad = input("edad: ")          
+address = input("address: ")          
+telef = input("telef: ")          
+
+persona = {"nombre": nombre, "edad": edad, "address": address, "telef": telef}
+print(persona["nombre"], "tiene ", persona["edad"], "de edad", ", el mismo vive en ", persona["address"], "y su # es: ", persona["telef"])
+
+
+lista = []
+
+u = 1
+while u <= 100:
+          lista.append(u)
+          u = u + 1
+print("el valor es: ",lista )
