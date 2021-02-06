34da2fe34929ff74f34ce3563857f7976443e5da
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-06 13:42:33 +0200

    ej

---
+++ in-out.py
@@ -0,0 +1,15 @@
+# entero = int(input("numero entero: "))
+# flotante = float(input("ingreasa flotante: "))
+# print(entero + flotante)
+
+
+
+print("*******")
+print("Ingresa nombres")
+print("*******\n")
+
+lista = []
+
+for i in range(4):
+          lista.append(input("ingresa los nombres: "))
+print(lista)          
