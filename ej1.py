32661d82bc0bfe0f5933a7871b314356b33f492c
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-06 11:56:10 +0200

    ejercicios funcionales

---
+++ ej1.py
@@ -0,0 +1,32 @@
+# pas = "pass"
+# password = ""
+# while password != pas:
+#           password = input("Introduce el pass: ")
+# print("All good")
+
+
+#Otro ejemplo
+# i = input("introduce: ")
+# for h in range(10):
+#           print(i)
+
+
+# edad = int(input("Ingresa tu edad: "))
+# for u in range(edad):
+#           print("Has cumplido " + str(u+1) + " years")
+
+
+# while True:
+#           frase = input("escribe: ")
+#           if frase == "salir":
+#                     break
+#           print(frase)
+
+
+year = int(input("Ingresa edad: "))
+salario = float(input("Ingresos mensuales: "))
+
+if year > 16 and salario >= 1000:
+          print("tienes que cotizar")
+else:
+    print("no tienes que hacer nada")          
