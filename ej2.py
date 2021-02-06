fbf4d74d9202aa36dadc4af5486af9b2358d6b61
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-06 16:40:57 +0200

    cheking

--- ej2.py
+++ ej2.py
@@ -4,6 +4,7 @@ print(asignatura)
 for i in asignatura:
           print("Estudio " + i + " en la escuela")
 
+esto = []
 nombre = input("nombre: ")
 edad = input("edad: ")          
 address = input("address: ")          
@@ -11,7 +12,9 @@ telef = input("telef: ")
 
 persona = {"nombre": nombre, "edad": edad, "address": address, "telef": telef}
 print(persona["nombre"], "tiene ", persona["edad"], "de edad", ", el mismo vive en ", persona["address"], "y su # es: ", persona["telef"])
-
+esto.append(persona)
+print("este es mi ejemplo: ",esto)
+print("**************************************************************")
 
 lista = []
 
