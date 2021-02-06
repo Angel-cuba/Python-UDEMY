abcd1fb1addd459f4eb93e7c8c1f274473637197
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-06 11:07:16 +0200

    while

---
+++ bucleWhile.py
@@ -0,0 +1,18 @@
+i = 0
+
+while i <= 10:
+          print("Hola mundo", i)
+          i += 1
+else:
+          print("Final del programa")   
+
+
+#Otro ejemplo
+
+edad = int(input("Ingresa una edad: "))
+
+while edad < 10:
+          print("Está mal tu edad")
+          edad = int(input("Ingresa tu edad nuevamente: "))
+else:
+    print("Este ciclo ha finalizado")          
