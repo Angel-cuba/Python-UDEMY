9ae9cfcb76b972ff743c47e219970a7fe79e60d3
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-06 10:53:21 +0200

    If, elif(else-if) and else

---
+++ sentencia_IF.py
@@ -0,0 +1,30 @@
+if True:
+          print("Es verdadero")
+else:
+              print("Nooooo")
+
+#Otro ejemplo
+
+user = int(input("Ingresa tu edad: "))
+if user < 20:
+          print("Welcome")
+else:
+    print("Cant go inside")
+    
+
+nota = int(input("Ingresa tu nota: "))
+if nota == 10:
+          print("perfecto")
+elif nota>= 9:
+          print("bastante buena")
+elif nota >= 8:
+          print("rango")
+elif nota >= 7:
+          print("Estudia")
+elif nota >= 6:
+          print("limite") 
+else:
+    print("final")                   
+                  
+              
+        
