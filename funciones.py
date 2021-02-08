58302629322d60f149bf650fc781e002bb43390d
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-08 17:17:11 +0200

    Definiciones de las funciones

---
+++ funciones.py
@@ -0,0 +1,19 @@
+def first():
+    print("Hola Angel")
+
+first()    
+
+#Fx
+def sumar():
+    i = 10
+    print(i)
+sumar()
+
+#fx
+def suma():
+          x = 20
+          y = 33
+          resultado = x+y
+          print(f"El resultado es : {resultado}" )
+
+suma()          
